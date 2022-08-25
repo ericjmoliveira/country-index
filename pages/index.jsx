3d6830c1098324c6be2ff25935c42a60a47492f1
Eric Oliveira <ericjmoliveira@gmail.com>
2022-08-25 15:37:13 -0300

    Fetch data from GraphQL API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,44 @@
-export default function Home() {
+import Head from 'next/head';
+import { gql } from '@apollo/client';
+
+import client from '../graphql/client';
+
+export async function getStaticProps() {
+    const { data } = await client.query({
+        query: gql`
+            {
+                countries {
+                    code
+                    name
+                    emoji
+                }
+            }
+        `
+    });
+
+    return {
+        props: {
+            countries: data.countries
+        }
+    };
+}
+
+export default function Home({ countries }) {
     return (
-        <div className="App">
-            <h1>App</h1>
-        </div>
+        <>
+            <Head>
+                <title>Country Index</title>
+            </Head>
+            <div>
+                <h1>Country Index</h1>
+                <ul>
+                    {countries.map((country) => (
+                        <li key={country.code}>
+                            {country.emoji} {country.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 }
